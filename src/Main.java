6e1589f2413e9683cd8723ca9579728ed9ddcd05
acidburnmonkey <acidburnmonkey@gmail.com>
2024-12-07 00:02:04 -0500

    wrapper and switches WIP

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,41 @@
+
 import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+
 public class Main {
+    public static void help(){
+        String str = "usage this is the help page";
+        System.out.println(str);
+    }
+
+    public static boolean switches(String zero){
+        if (zero.equals("-v")){
+            return true;
+        }
+        else if (zero.equals("-h")) {
+            help();
+        return false;
+        }
+        else{
+            return false;
+        }
+    }
+
     public static void main(String[] args) {
+
+        String[] aggs = {"-v","two ", "third" };
+//        String[] aggs = {};
+
+        if (aggs.length == 0 | aggs.length >3 ) {
+            help();
+            return;
+        }
+
+        //Checks for -v , false by default
+        boolean verbose = switches(aggs[0]);
+
 
         // ORIGINAL FILE
         String file1 = "here/second/newdir";
@@ -39,5 +71,8 @@ public class Main {
             System.out.println("File not Found");
         }
 
+
     }
+
+
 }
