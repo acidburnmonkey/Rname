503be7f9c15a281998422102e71b3fbda8695782
acidburnmonkey <acidburnmonkey@gmail.com>
2024-12-07 01:23:45 -0500

    idk

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,18 @@
 import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
+import java.util.Scanner;
 
 public class Main {
+
+    // HELP Switch
     public static void help(){
         String str = "usage this is the help page";
         System.out.println(str);
     }
 
+
+    // Checks for switches -v or -h
     public static boolean switches(String zero){
         if (zero.equals("-v")){
             return true;
@@ -23,12 +27,54 @@ public class Main {
         }
     }
 
+
+    // Does the renaming
+    public static void renamingOperation(String[] args){
+        boolean ok;
+
+        // ORIGINAL FILE
+        String file1 = args[0].trim();
+        File OriginalFile = new File(file1);
+        // Builds the parent path
+        Path path = Paths.get(file1);
+        path = path.getParent();
+
+        // RENAME To
+        var user = path.resolve(args[1].trim());
+        File newName = new File(user.toString());
+
+
+        //rename operation
+        if (OriginalFile.exists() & OriginalFile.isFile()) {
+            // Check if already exist
+            if (OriginalFile.getPath().equals(newName.getPath())){
+                System.out.println("files are same");
+            }
+            System.out.println(" File exist");
+            OriginalFile.renameTo(newName);
+
+        }
+
+        //checks for directory !NOT IMPLEMENTED 🤡
+        /*else if (OriginalFile.exists() & OriginalFile.isDirectory()) {
+            System.out.println(" Directory exist");
+
+            // RENAME To
+            var user = path.resolve("old");
+            File file2 = new File(user.toString());
+            OriginalFile.renameTo(file2);
+        } else {
+            System.out.println("File not Found");
+        }*/
+    }
+
+ // MAIN
     public static void main(String[] args) {
 
-        String[] aggs = {"-v","two ", "third" };
+        String[] aggs = {"here/second/old/new", "new", "third"};
 //        String[] aggs = {};
 
-        if (aggs.length == 0 | aggs.length >3 ) {
+        if (aggs.length == 0 | aggs.length > 3) {
             help();
             return;
         }
@@ -36,43 +82,23 @@ public class Main {
         //Checks for -v , false by default
         boolean verbose = switches(aggs[0]);
 
+       try {
+           if (!verbose) {
+               System.out.println(aggs[0]);
 
-        // ORIGINAL FILE
-        String file1 = "here/second/newdir";
-        File OriginalFile = new File(file1);
+               renamingOperation(aggs);
 
-        // Builds the parent path
-        Path path = Paths.get(file1);
-        path = path.getParent();
+           }//end if
+           else {
+               //copy args and pass it 1:2
 
-//        System.out.println(path.resolve("aaa"));
+               System.out.println(aggs[1] + " -> " );
+           }
 
-
-        if ( OriginalFile.exists() & OriginalFile.isFile() ){
-            System.out.println(" File exist");
-
-            // RENAME To
-            var user =  path.resolve("renamed");
-            File file2 = new File(user.toString());
-            OriginalFile.renameTo(file2);
-
-        }
-
-        else if ( OriginalFile.exists() & OriginalFile.isDirectory() ){
-            System.out.println(" Directory exist");
-
-            // RENAME To
-            var user =  path.resolve("old");
-            File file2 = new File(user.toString());
-            OriginalFile.renameTo(file2);
-        }
-
-        else{
-            System.out.println("File not Found");
-        }
-
+       }
+       catch (Exception e){
+           System.out.println("Something crashed :" + e.getMessage());
+       }
 
     }
-
-
 }
